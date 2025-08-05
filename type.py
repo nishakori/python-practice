db313e7b782ddefbfbec89b5d89de6e25119f526
nisha kori <korinisha25@gmail.com>
2025-08-05 13:16:37 +0530

    Add files via upload

---
+++ type.py
@@ -0,0 +1,12 @@
+
+
+a = "3.12"
+n = type(a)
+print (n)     
+a= int(input("Enter number 1: "))
+
+b = int(input("Enter number 2: "))
+
+print("Number a is: ", a)
+print("Number b is: ", b) 
+print("Sum is ", a + b)
